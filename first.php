acb7c61c90bbc66fde2836bf6dbfd3c85f43816a
drex01 <112757033+drex01@users.noreply.github.com>
2022-09-03 04:42:04 -0700

    Add files via upload

---
+++ first.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<center>
+		<h1>Saint Joseph College</h1>
+		
+		<?php
+			for($x=1; $x<=6; $x++){
+			echo "<h$x>Saint Joseph College</h$x>";
+			}
+		?>
+	</center>
+	</body>
+</html>
